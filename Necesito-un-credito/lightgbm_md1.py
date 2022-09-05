8a159cd194b7cebce18ee45630e6c9e295ccb4b1
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-09-05 01:36:00 +0000

    fixing bug in line 90

--- Necesito-un-credito/lightgbm_md1.py
+++ Necesito-un-credito/lightgbm_md1.py
@@ -87,7 +87,7 @@ opt_cutoff = roc_auc_cutoff(Y, lightgbm_pred_train)
 print('The optimal cutoff is', opt_cutoff)
 
 ## Predicting on the test
-lightgbm_pred_test = LightGBM_md.predict_proba(test.drop(columns = ['Id'], axis = 1))[:, 1]
+lightgbm_pred_test = LightGBM_md.predict_proba(test)[:, 1]
 lightgbm_label_test = np.where(lightgbm_pred_test < opt_cutoff, 0, 1)
 
 ## Data-frame for submission

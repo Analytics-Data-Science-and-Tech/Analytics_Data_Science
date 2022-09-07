41e39b74b8f517af6808951f1006600006af1fdb
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-09-07 14:32:12 +0000

    updating script

--- Covid-Tweets/RF_md.py
+++ Covid-Tweets/RF_md.py
@@ -7,8 +7,8 @@ from sklearn.ensemble import RandomForestClassifier
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
 
-# test_id = test['Id']
-# test = test.drop(columns = ['Id', 'text', 'reply_to_screen_name', 'hashtags'], axis = 1)
+test_id = test['Id']
+test = test.drop(columns = ['Id', 'text', 'reply_to_screen_name', 'hashtags'], axis = 1)
 
 ## Defining input and target
 X = train.drop(columns = ['text', 'reply_to_screen_name', 'hashtags', 'country'], axis = 1)
@@ -19,19 +19,16 @@ Y = np.where(Y == 'us', 0,
                                np.where(Y == 'australia', 3,
                                         np.where(Y == 'ireland', 4, 5)))))
 
-## Defining the hyper-parameter grid
-LightGBM_param_grid = {'n_estimators': [300],
-                       'max_depth': [5],
-                       'num_leaves': [20],
-                       'min_data_in_leaf': [15],
-                       'learning_rate': [0.01],
-                       'feature_fraction': [0.8],
-                       'lambda_l1': [0],
-                       'lambda_l2': [0]
-                      }
+## Defining hyper-paramerters for RF
+RF_param_grid = {'n_estimators': [500],
+                 'estimator__min_samples_split': [10, 15],
+                 'estimator__min_samples_leaf': [5, 7],
+                 'estimator__max_depth' : [3, 5, 7]}
+
 
 ## Building the multi-classifier  
-one_vs_all_LightGBM = OneVsRestClassifier(estimator = LGBMClassifier(*LightGBM_param_grid)).fit(X, Y)
+RF_md = OneVsRestClassifier(estimator = RandomForestClassifier())
+one_vs_all_RF = GridSearchCV(RF_md, RF_param_grid, scoring = 'accuracy', n_jobs = -1, verbose = 3).fit(X, Y)
 
 ## Predicting on the test
 one_vs_all_LightGBM_pred = one_vs_all_RF.predict_proba(test)

39a4269a391f443e346cde522a3e4cc8b94b620c
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-09-07 14:35:51 +0000

    fixing bug in hyper-parameter dict

--- Covid-Tweets/RF_md.py
+++ Covid-Tweets/RF_md.py
@@ -20,7 +20,7 @@ Y = np.where(Y == 'us', 0,
                                         np.where(Y == 'ireland', 4, 5)))))
 
 ## Defining hyper-paramerters for RF
-RF_param_grid = {'n_estimators': [500],
+RF_param_grid = {'estimator__n_estimators': [500],
                  'estimator__min_samples_split': [10, 15],
                  'estimator__min_samples_leaf': [5, 7],
                  'estimator__max_depth' : [3, 5, 7]}
@@ -31,14 +31,14 @@ RF_md = OneVsRestClassifier(estimator = RandomForestClassifier())
 one_vs_all_RF = GridSearchCV(RF_md, RF_param_grid, scoring = 'accuracy', n_jobs = -1, verbose = 3).fit(X, Y)
 
 ## Predicting on the test
-one_vs_all_LightGBM_pred = one_vs_all_RF.predict_proba(test)
-one_vs_all_LightGBM_pred = np.argmax(one_vs_all_LightGBM_pred, axis = 1)
+one_vs_all_RF_pred = one_vs_all_RF.predict_proba(test)
+one_vs_all_RF_pred = np.argmax(one_vs_all_RF_pred, axis = 1)
 
-data_out = pd.DataFrame({'Id': test_id, 'Category': one_vs_all_LightGBM_pred})
+data_out = pd.DataFrame({'Id': test_id, 'Category': one_vs_all_RF_pred})
 data_out['Category'] = np.where(data_out['Category'] == 0, 'us',
                                 np.where(data_out['Category'] == 1, 'uk',
                                          np.where(data_out['Category'] == 2, 'canada',
                                                   np.where(data_out['Category'] == 3, 'australia',
                                                            np.where(data_out['Category'] == 4, 'ireland', 'new_zealand')))))
 
-data_out.to_csv('LightGBM_submission_md.csv', index = False)
+data_out.to_csv('RF_submission_3.csv', index = False)

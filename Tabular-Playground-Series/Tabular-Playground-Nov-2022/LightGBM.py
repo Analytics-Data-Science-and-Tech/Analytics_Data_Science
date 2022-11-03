0312ff91041a1eb2efd521b2f426bf42ada274e9
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-11-03 00:25:26 +0000

    updating

--- Tabular-Playground-Series/Tabular-Playground-Nov-2022/LightGBM.py
+++ Tabular-Playground-Series/Tabular-Playground-Nov-2022/LightGBM.py
@@ -31,10 +31,10 @@ test = preds_df[preds_df['target'].isnull()]
 ## Reading logloss data 
 logloss_data = pd.read_csv('logloss_data.csv')
 
-X = train[logloss_data['File'][0:300].values]
+X = train[logloss_data['File'][0:500].values]
 Y = train['target']
 
-test_new = test[logloss_data['File'][0:300].values]
+test_new = test[logloss_data['File'][0:500].values]
 
 ## Defining list to store results
 lgb_results, test_preds_lgb = list(), list()
@@ -79,4 +79,4 @@ print(test_preds_lgb.head(5))
 submission['pred'] = test_preds_lgb
 print(submission.head())
 
-submission.to_csv('submission_LightGBM_300.csv', index = False)
+submission.to_csv('submission_LightGBM_500.csv', index = False)

551b0f512ddd2cf3cd44ec7db4e74a4f7a2c2233
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-11-07 13:31:03 +0000

    updating script

--- Tabular-Playground-Series/Tabular-Playground-Nov-2022/Removing_Correlated_Models.py
+++ Tabular-Playground-Series/Tabular-Playground-Nov-2022/Removing_Correlated_Models.py
@@ -5,14 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import log_loss
 from sklearn.model_selection import StratifiedKFold
 from lightgbm import LGBMClassifier
 
 ## Reading data-files
-df = pd.read_parquet('s3://analytics-data-science-competitions/Tabular-Playground-Series/Tabular-Playground-Nov-2022/preds_concat_gzip.parquet', engine = 'fastparquet')
+df = pd.read_parquet('s3://analytics-data-science-competitions/Tabular-Playground-Series/Tabular-Playground-Nov-2022/preds_logit_concat_gzip.parquet', engine = 'fastparquet')
 
 ## train and test
-preds_df = df.clip(0, 1) 
-train = preds_df[preds_df['target'].notnull()]
-test = preds_df[preds_df['target'].isnull()] 
+# preds_df = df.clip(0, 1) 
+# train = preds_df[preds_df['target'].notnull()]
+# test = preds_df[preds_df['target'].isnull()] 

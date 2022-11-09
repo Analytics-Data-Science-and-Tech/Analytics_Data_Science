e9382621d4b03e3300f76af82bb0893588f96f34
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-11-09 15:08:44 +0000

    updating script

--- Tabular-Playground-Series/Tabular-Playground-Nov-2022/Run_LightGBM.py
+++ Tabular-Playground-Series/Tabular-Playground-Nov-2022/Run_LightGBM.py
@@ -115,3 +115,25 @@ print('The average log-loss over 5-fold CV is', np.mean(lgb_results))
 ## Weighted average of fold predictions ##
 ##########################################
 
+w1 = 1/ lgb_results[0]
+w2 = 1/ lgb_results[1]
+w3 = 1/ lgb_results[2]
+w4 = 1/ lgb_results[3]
+w5 = 1/ lgb_results[4]
+w_tot = w1 + w2 + w3+ w4 + w5
+w1 = w1 / w_tot
+w2 = w2 / w_tot
+w3 = w3 / w_tot
+w4 = w4 / w_tot
+w5 = w5 / w_tot
+
+pred1 = w1*test_preds_lgb_fold_1[0]
+pred2 = w2*test_preds_lgb_fold_2[0]
+pred3 = w3*test_preds_lgb_fold_3[0]
+pred4 = w4*test_preds_lgb_fold_4[0]
+pred5 = w4*test_preds_lgb_fold_5[0]
+
+submission['pred'] = pred1 + pred2 + pred3 + pred4 + pred5
+
+
+

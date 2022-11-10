3bdda74e6847b28e19f02ecce6f7ad3c2b06af64
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2022-11-10 01:48:21 +0000

    updating

--- Tabular-Playground-Series/Tabular-Playground-Nov-2022/Run_LightGBM.py
+++ Tabular-Playground-Series/Tabular-Playground-Nov-2022/Run_LightGBM.py
@@ -63,15 +63,15 @@ test_new = test[to_select.values]
 
 CV_scores = list()    
     
-for i in range(0, 500):
+for i in range(0, 10):
     
     print('Working in', i, ' Run')
     run = Run_LightGBM(X, Y, test_new, submission)
-    CV_scores.append(run[0])
-    location_name = 's3://analytics-data-science-competitions/Tabular-Playground-Series/Tabular-Playground-Nov-2022/LightGBM_Preds/' + 'LightGBM_run_' + str(i) + '.csv'
-    run[1].to_csv(location_name, index = False)
+#     CV_scores.append(run[0])
+#     location_name = 's3://analytics-data-science-competitions/Tabular-Playground-Series/Tabular-Playground-Nov-2022/LightGBM_Preds/' + 'LightGBM_run_' + str(i) + '.csv'
+#     run[1].to_csv(location_name, index = False)
     
     
-CV_scores = pd.DataFrame({'Run': [i for i in range(0, 500)], 'CV_score': CV_scores})
-CV_scores.to_csv('s3://analytics-data-science-competitions/Tabular-Playground-Series/Tabular-Playground-Nov-2022/LightGBM_Preds/CV_scores.csv', index = False)
+# CV_scores = pd.DataFrame({'Run': [i for i in range(0, 500)], 'CV_score': CV_scores})
+# CV_scores.to_csv('s3://analytics-data-science-competitions/Tabular-Playground-Series/Tabular-Playground-Nov-2022/LightGBM_Preds/CV_scores.csv', index = False)
 

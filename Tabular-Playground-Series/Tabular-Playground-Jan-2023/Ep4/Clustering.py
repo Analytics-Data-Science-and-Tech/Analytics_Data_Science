cb3d2005fb82c5ae5fdabfa487907966a396fad4
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2023-01-26 14:16:32 +0000

    adding script

---
+++ Tabular-Playground-Series/Tabular-Playground-Jan-2023/Ep4/Clustering.py
@@ -0,0 +1,69 @@
+import boto3
+import pandas as pd; pd.set_option('display.max_columns', 100)
+import numpy as np
+
+import matplotlib.pyplot as plt; plt.style.use('ggplot')
+import seaborn as sns
+
+from scipy.stats import rankdata
+from sklearn.cluster import KMeans
+from sklearn.tree import DecisionTreeRegressor, plot_tree
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import KFold, train_test_split, GridSearchCV, StratifiedKFold, TimeSeriesSplit
+from sklearn.metrics import mean_squared_error, roc_auc_score, davies_bouldin_score, calinski_harabasz_score, silhouette_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+# from lightgbm import LGBMClassifier
+# from xgboost import XGBClassifier
+# from catboost import CatBoostClassifier
+
+# import optuna as op
+
+s3 = boto3.resource('s3')
+bucket_name = 'analytics-data-science-competitions'
+bucket = s3.Bucket(bucket_name)
+
+file_key_1 = 'Tabular-Playground-Series/Tabular-Playground-Jan-2023-4/train.csv'
+file_key_2 = 'Tabular-Playground-Series/Tabular-Playground-Jan-2023-4/test.csv'
+file_key_3 = 'Tabular-Playground-Series/Tabular-Playground-Jan-2023-4/sample_submission.csv'
+
+bucket_object_1 = bucket.Object(file_key_1)
+file_object_1 = bucket_object_1.get()
+file_content_stream_1 = file_object_1.get('Body')
+
+bucket_object_2 = bucket.Object(file_key_2)
+file_object_2 = bucket_object_2.get()
+file_content_stream_2 = file_object_2.get('Body')
+
+bucket_object_3 = bucket.Object(file_key_3)
+file_object_3 = bucket_object_3.get()
+file_content_stream_3 = file_object_3.get('Body')
+
+## Reading data files
+train = pd.read_csv(file_content_stream_1)
+test = pd.read_csv(file_content_stream_2)
+submission = pd.read_csv(file_content_stream_3)
+
+scaler = MinMaxScaler()
+X_trans = scaler.fit_transform(X)
+
+## Defining the number of clusters to be considered
+krange = list(range(2, 4))
+
+## Defining lists to store scores
+CH_scores, DB_scores, silhouette_scores = list(), list(), list()
+inertias = list()
+
+for i in krange:
+    print(i)
+    ## Here we define the k-means model
+    cluster_md = KMeans(n_clusters = i, n_init = 20).fit(X_trans)
+    cluster_assignments = cluster_md.labels_
+    inertias.append(cluster_md.inertia_)
+    
+plt.plot(krange, inertias)
+plt.xlabel('Number of Clusters (k)')
+plt.ylabel('Custer Inertia')
+plt.show()
+fig.savefig('V1_V28_clusters.pdf')

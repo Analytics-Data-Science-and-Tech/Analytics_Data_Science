505eef793dbe838d58e0bdc53c1f8ade9d56bfdd
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2023-01-26 22:51:25 +0000

    updating script

--- Tabular-Playground-Series/Tabular-Playground-Jan-2023/Ep4/Clustering.py
+++ Tabular-Playground-Series/Tabular-Playground-Jan-2023/Ep4/Clustering.py
@@ -51,7 +51,7 @@ scaler = MinMaxScaler()
 X_trans = scaler.fit_transform(X)
 
 ## Defining the number of clusters to be considered
-krange = list(range(2, 100))
+krange = list(range(2, 200))
 
 ## Defining lists to store scores
 CH_scores, DB_scores, silhouette_scores = list(), list(), list()
@@ -65,6 +65,9 @@ for i in krange:
     inertias.append(cluster_md.inertia_)
     
 plt.figure(figsize = (10, 8))
+
+data_out = pd.DataFrame({'cluster': krange, 'inertia': inertias})
+data_out.to_csv('cluster_inertias.csv', index = False)
     
 plt.plot(krange, inertias)
 plt.xlabel('Number of Clusters (k)')

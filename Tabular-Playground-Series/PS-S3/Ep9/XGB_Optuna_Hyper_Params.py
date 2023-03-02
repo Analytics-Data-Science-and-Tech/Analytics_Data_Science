bb84c969a9df0d425838c6c48458a26969a2c99d
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2023-03-02 12:07:40 +0000

    updating script

--- Tabular-Playground-Series/PS-S3/Ep9/XGB_Optuna_Hyper_Params.py
+++ Tabular-Playground-Series/PS-S3/Ep9/XGB_Optuna_Hyper_Params.py
@@ -49,3 +49,60 @@ train_no_dup = pd.DataFrame(train_no_dup.groupby(train_no_dup.columns.tolist()[0
 X = train_no_dup.drop(columns = ['Strength'], axis = 1)
 Y = train_no_dup['Strength']
 
+############
+## Optuna ##
+############
+
+print('------------------------------------')
+print(' (-: Optuna Optimization Started :-)')
+print('------------------------------------')
+
+class Objective:
+
+    def __init__(self, seed):
+        # Hold this implementation specific arguments as the fields of the class.
+        self.seed = seed
+
+    def __call__(self, trial):
+        
+        ## Parameters to be evaluated
+        param = dict(tree_method = 'hist', 
+                     max_depth = trial.suggest_int('max_depth', 2, 10),
+                     learning_rate = trial.suggest_float('learning_rate', 1e-4, 1e-1, log = True),
+                     n_estimators = trial.suggest_int('n_estimators', 30, 10000),
+                     gamma = trial.suggest_float('gamma', 0, 10),
+                     min_child_weight = trial.suggest_int('min_child_weight', 1, 100),
+                     colsample_bytree = trial.suggest_float('colsample_bytree', 0.2, 0.9),
+                     subsample = trial.suggest_float('subsample', 0.2, 0.9)
+#                      device = 'gpu'
+                    )
+
+        scores = []
+        
+        skf = KFold(n_splits = 5, shuffle = True, random_state = self.seed)
+
+        for train_idx, valid_idx in skf.split(X, Y):
+
+            X_train, X_valid = X.iloc[train_idx], X.iloc[valid_idx]
+            Y_train , Y_valid = Y.iloc[train_idx] , Y.iloc[valid_idx]
+
+            model = XGBRegressor(**param).fit(X_train, Y_train)
+
+            preds_valid = model.predict(X_valid)
+
+            score = mean_squared_error(Y_valid, preds_valid, squared = False)
+            scores.append(score)
+
+        return np.mean(scores)
+    
+## Defining SEED and Trials
+SEED = 42
+N_TRIALS = 70
+
+# Execute an optimization
+study = optuna.create_study(direction = 'minimize')
+study.optimize(Objective(SEED), n_trials = N_TRIALS)
+
+optuna_hyper_params = pd.DataFrame.from_dict([study.best_trial.params])
+file_name = 'XGB_Seed_' + str(SEED) + '_Optuna_Hyperparameters.csv'
+optuna_hyper_params.to_csv(file_name, index = False)

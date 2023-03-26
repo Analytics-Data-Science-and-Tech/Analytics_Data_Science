c3c9fcf895fe2491b92d0d3186fdac3a85054bc9
Oscar Aguilar <oscar.aguilar.1987@gmail.com>
2023-03-26 13:33:51 +0000

    updating script

---
+++ Pog-Series/Rob-Sleep-Prediction/Run_XGBoost_Optuna.py
@@ -0,0 +1,195 @@
+import boto3
+import pandas as pd; pd.set_option('display.max_columns', 100)
+import numpy as np
+
+from tqdm import tqdm
+
+import holidays
+
+from functools import partial
+import scipy as sp
+
+import matplotlib.pyplot as plt; plt.style.use('ggplot')
+import seaborn as sns
+
+from scipy.stats import rankdata
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.tree import DecisionTreeRegressor, plot_tree
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import KFold, train_test_split, GridSearchCV, StratifiedKFold, TimeSeriesSplit, GroupKFold
+from sklearn.metrics import mean_squared_error, roc_auc_score, log_loss
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor, HistGradientBoostingClassifier, HistGradientBoostingRegressor, GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression, LinearRegression, Lasso, Ridge, ElasticNet
+from lightgbm import LGBMClassifier, LGBMRegressor 
+from xgboost import XGBClassifier, XGBRegressor
+from catboost import CatBoostClassifier, CatBoostRegressor
+
+import optuna 
+
+s3 = boto3.resource('s3')
+bucket_name = 'analytics-data-science-competitions'
+bucket = s3.Bucket(bucket_name)
+
+file_key_1 = 'Tabular-Playground-Series/Pog-Series/Rob-Sleep-Prediction/train.csv'
+file_key_2 = 'Tabular-Playground-Series/Pog-Series/Rob-Sleep-Prediction/test.csv'
+file_key_3 = 'Tabular-Playground-Series/Pog-Series/Rob-Sleep-Prediction/sample_submission.csv'
+
+bucket_object_1 = bucket.Object(file_key_1)
+file_object_1 = bucket_object_1.get()
+file_content_stream_1 = file_object_1.get('Body')
+
+bucket_object_2 = bucket.Object(file_key_2)
+file_object_2 = bucket_object_2.get()
+file_content_stream_2 = file_object_2.get('Body')
+
+bucket_object_3 = bucket.Object(file_key_3)
+file_object_3 = bucket_object_3.get()
+file_content_stream_3 = file_object_3.get('Body')
+
+## Reading data files
+train = pd.read_csv(file_content_stream_1)
+train['date'] = pd.to_datetime(train['date'])
+
+test = pd.read_csv(file_content_stream_2)
+test['date'] = pd.to_datetime(test['date'])
+
+submission = pd.read_csv(file_content_stream_3)
+
+def get_holidays(df):
+    years_list = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
+
+    holiday_US = holidays.CountryHoliday('US', years = years_list)
+    holiday_dict = holiday_US.copy()
+
+    df['holiday_name'] = df['date'].map(holiday_dict)
+    df['is_holiday'] = np.where(df['holiday_name'].notnull(), 1, 0)
+    df['holiday_name'] = df['holiday_name'].fillna('Not Holiday')
+    
+    return df.drop(columns = ['holiday_name'])
+
+
+def feature_engineer(df):
+    
+    new_df = df.copy()
+    new_df["month"] = df["date"].dt.month
+    new_df["month_sin"] = np.sin(new_df['month'] * (2 * np.pi / 12))
+#     new_df["month_cos"] = np.cos(new_df['month'] * (2 * np.pi / 12))
+    
+    new_df["day"] = df["date"].dt.day
+    new_df["day_sin"] = np.sin(new_df['day'] * (2 * np.pi / 12))
+#     new_df["day_cos"] = np.cos(new_df['day'] * (2 * np.pi / 12))
+    
+    new_df["day_of_week"] = df["date"].dt.dayofweek
+    new_df["day_of_week"] = new_df["day_of_week"].apply(lambda x: 0 if x <= 3 else(1 if x == 4 else (2 if x == 5 else (3))))
+    
+    new_df["day_of_year"] = df["date"].dt.dayofyear
+    new_df["year"] = df["date"].dt.year
+    
+    return new_df
+
+train = feature_engineer(train)
+test = feature_engineer(test)
+
+train = get_holidays(train)
+test = get_holidays(test)
+
+train.loc[((train['date'] >= '2017-09-27') & (train['date'] <= '2018-06-12')), 'sleep_hours'] = train.loc[((train['date'] >= '2017-09-27') & (train['date'] <= '2018-06-12')), 'sleep_hours'] / 1.94 
+
+X = train.drop(columns = ['date', 'sleep_hours', 'year'], axis = 1)
+Y = train['sleep_hours']
+
+test = test.drop(columns = ['date', 'sleep_hours', 'year'], axis = 1)
+
+train = train[train['date'] > '2015-07-20'].reset_index(drop = True)
+
+#########################
+## Optuna Optimization ##
+#########################
+
+print('-----------------------------')
+print(' (-: Optuna has started :-) ')
+print('-----------------------------')
+
+class Objective:
+
+    def __init__(self, seed):
+        # Hold this implementation specific arguments as the fields of the class.
+        self.seed = seed
+
+    def __call__(self, trial):
+        
+        ## Parameters to be evaluated
+        param = dict(tree_method = 'hist', 
+                     max_depth = trial.suggest_int('max_depth', 2, 15),
+                     learning_rate = trial.suggest_float('learning_rate', 1e-4, 1e-1, log = True),
+                     n_estimators = trial.suggest_int('n_estimators', 30, 5000),
+                     gamma = trial.suggest_float('gamma', 0, 10),
+                     min_child_weight = trial.suggest_int('min_child_weight', 1, 100),
+                     colsample_bytree = trial.suggest_float('colsample_bytree', 0.2, 0.9),
+                     subsample = trial.suggest_float('subsample', 0.2, 0.9)
+                    )
+
+        scores = []
+        
+        skf = KFold(n_splits = 30, shuffle = True, random_state = self.seed)
+
+        for train_idx, valid_idx in skf.split(X, Y):
+
+            X_train, X_valid = X.iloc[train_idx], X.iloc[valid_idx]
+            Y_train , Y_valid = Y.iloc[train_idx] , Y.iloc[valid_idx]
+
+            model = XGBRegressor(**param).fit(X_train, Y_train)
+
+            preds_valid = model.predict(X_valid)
+
+            score = mean_squared_error(Y_valid, preds_valid, squared = False)
+            scores.append(score)
+
+        return np.mean(scores)
+    
+## Defining SEED and Trials
+SEED = 42
+N_TRIALS = 70
+
+# Execute an optimization
+study = optuna.create_study(direction = 'minimize')
+study.optimize(Objective(SEED), n_trials = N_TRIALS)
+
+optuna_hyper_params = pd.DataFrame.from_dict([study.best_trial.params])
+file_name = 'XGBoost_Seed_' + str(SEED) + '_Optuna_Hyperparameters.csv'
+optuna_hyper_params.to_csv(file_name, index = False)
+
+print('----------------------------')
+print(' (-: Starting CV process :-)')
+print('----------------------------')
+
+xgb_cv_scores, preds = list(), list()
+
+for i in tqdm(range(5)):
+
+    skf = KFold(n_splits = 30, random_state = SEED, shuffle = True)
+    
+    for train_ix, test_ix in skf.split(X, Y):
+        
+        ## Splitting the data 
+        X_train, X_test = X.iloc[train_ix], X.iloc[test_ix]
+        Y_train, Y_test = Y.iloc[train_ix], Y.iloc[test_ix]
+                
+        ## Building XGBoost model
+        xgb_md = XGBRegressor(**study.best_trial.params).fit(X_train, Y_train)
+        
+        ## Predicting on X_test and test
+        xgb_pred_1 = xgb_md.predict(X_test)
+        xgb_pred_2 = xgb_md.predict(test)
+        
+        ## Computing rmse
+        xgb_cv_scores.append(mean_squared_error(Y_test, xgb_pred_1, squared = False))
+        preds.append(xgb_pred_2)
+
+xgb_cv_score = np.mean(xgb_cv_scores)    
+print('The average oof rmse score over 5-folds (run 5 times) is:', xgb_cv_score)
+
+xgb_preds = pd.DataFrame(preds).mean(axis = 0)
+submission['sleep_hours'] =  xgb_preds
+submission.to_csv('xgb_baseline_optuna_submission.csv', index = False)
